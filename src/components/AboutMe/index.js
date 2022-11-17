6a392dfab85fe6a9f9fd4b4ed1667911328e9115
Pogga1 <Andreweysoldt@gmail.com>
2022-11-16 18:23:13 -0800

    text changes

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,8 +14,8 @@ export default function AboutMe() {
           </div>
           <div className="card col" styles={styles.card}>
             <div className="card-body">
-              <h5 className="card-title">Education</h5>
-              <p className="card-text">I am currently a student at the Northwestern Full-Stack Web Development Bootcamp. Previously, I graduated from UIC with a degree in Integrated Health Studies with a concentration in Health Sciences. I graduated in December 2021.</p>
+              <h5 className="card-title"></h5>
+              <p className="card-text"></p>
               <div className='imgContainer'>
                 <img src={UICimg} alt="educationImg" className='uicIMG'></img>
                 <img src={NWimg} alt="educationImg" className='nwIMG'></img>
@@ -24,14 +24,14 @@ export default function AboutMe() {
           </div>
           <div className="card col">
             <div className="card-body">
-              <h5 className="card-title">Career Path</h5>
-              <p className="card-text"> I was working towards becoming a doctor, as that had always been a life-long dream of mine. I realized though that the medicine lifestyle was not for me, so I decided to make a career switch into coding. I feel this is the best profession for me and am really excited to get my career started.</p>
+              <h5 className="card-title"></h5>
+              <p className="card-text"> </p>
             </div>
           </div>
           <div className="card col">
             <div className="card-body">
-              <h5 className="card-title">Right Now</h5>
-              <p className="card-text"> I have completed many projects now that I started my coding career. They can be seen in the Portfolio section of this page. If you have any more questions about me, feel free to contact me through the Contact page. My email and Github are also linked below.
+              <h5></h5>
+              <p className="card-text"> 
                 Thank you- Hamza!</p>
             </div>
           </div>
