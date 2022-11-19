9cbd102bd7c916deb221eda5a1d1db95023ba94e
Pogga1 <Andreweysoldt@gmail.com>
2022-11-19 13:45:36 -0800

    finished styling of contact page and fixed footer

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,22 +18,24 @@ function Contact() {
   };
 
   return (
-    <div>
+    <div className='contactForm row'>
       <h1>Contact Page</h1>
       <form onSubmit={sendEmail}>
         <div className="form-group">
           <label for="user_email">Email address</label>
-          <input type="email" className="form-control" id="user_email" placeholder="name@example.com" />
+          <input type="email" className="form-control" id="user_email"  />
         </div>
         <div className="form-group">
           <label for="user_name">Name</label>
-          <input type="text" className="form-control" id="user_name" placeholder="First and Last Name" />
+          <input type="text" className="form-control" id="user_name"  />
         </div>
         <div className="form-group">
           <label for="user-message">Message</label>
-          <textarea className="form-control" id="user-message" rows="6"></textarea>
+          <textarea className="form-control" id="user-message" ></textarea>
         </div>
-        <button type='submit' className="contactFormBtn" onSubmit={sendEmail} Submit/>
+        <div id='buttonContainer'>
+        <button type='submit' className="contactFormBtn" onSubmit={sendEmail} Submit>Submit</button>
+        </div>
       </form>
     </div>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Footer() {
 
   return (
-    <footer className="footer d-flex justify-start mx-5 position-relative bottom-0 mt-4">
+    <footer className="footer d-flex mx-5 position-fixed bottom-0 mt-4">
       <div>
-        <h5> Thanks for Visiting!</h5>
+        <h5> If your interested:</h5>
       </div>
       <div>
       <a href="https://github.com/Pogga1" target='_blank' className='mx-4'>Github</a>
