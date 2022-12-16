9f495812d092c5fe29c5db94c74ad5784d6d9df4
Pogga1 <Andreweysoldt@gmail.com>
2022-12-16 12:04:00 -0800

    finishing up portfolio, fixed resume, about me, contact, and project section

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -7,14 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AboutMe() {
     return (
-      <div className='AboutMe '>
-        <div className='d-flex justify-content-center'>
-          <h2>About Me</h2>
-        </div>
+      <div className='AboutMe d-flex'>
         <div className='infoContainer'>
-          <div>content</div>
-          
-          
+          <h2 className='d-flex justify-content-center header'>About Me</h2>
+
+          <div>
+            Welcome to my portfolio where you can find information about me, my projects, and contact info! 
+              To start off, my name is Andrew Eysoldt and I am a full stack web developer from Tucson, Arizona. I am very hardworking and passionate about learning new things. I aim to continue improving in my coding skills and further hone the skills i have learned. My favorite hobbies include watching movies and playing video games.Thankyou for visiting my page!
+          </div>
+           <h3 className='movies'>My favorite movies</h3>
+           <div>
+            <img src=''></img>
+            <img src=''></img>
+           </div>
         </div>
         </div>
     );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import "./Resume.css";
 import { SiHtml5, SiJava, SiCss3, SiBootstrap, SiTailwindcss, SiMongodb, SiExpress, SiMysql, SiNodedotjs, SiReact, SiJquery, SiHeroku, SiGit, SiNpm, SiHandlebarsdotjs } from 'react-icons/si'
-
+import resume from '../../assets/Resume-Andrew-Eysoldt-1.pdf'
 export default function Resume() {
   return (
     <div className="resume">
-      <h1 className="d-flex justify-content-center mt-4 highlights">Resume</h1>
-      <p className="card-text text-center">
-                <h2>
-                <ul>
-                  <li> <SiHtml5/>  <SiJava/> <SiCss3/> <SiBootstrap/> <SiTailwindcss/> <SiMongodb/> <SiExpress/> <SiMysql/> <SiNodedotjs/> <SiReact/> <SiJquery/> <SiHeroku/> <SiGit/> <SiNpm/> <SiHandlebarsdotjs/></li>
-                </ul>
-                </h2>
-              </p>
+        <div  className="container d-flex justify-content-center">
+          <a className='button rounded' href={resume} target='_blank'>View Resume Here</a>
+        </div>
+      <h1 className=" justify-content-center d-flex mt-4 highlights">My Skills</h1>
+      <div className="skill-container text-center">
+                <h1 className="Skills mx-5">
+               <SiHtml5/> Html
+               <SiJava/> JavaScript
+               <SiCss3/> CSS
+               <SiBootstrap/> Bootstrap
+               <SiTailwindcss/> Tailwind
+               <SiMongodb/> MongoDb
+               <SiExpress/> Express
+               <SiMysql/> MySql
+               <SiNodedotjs/> Node.js
+               <SiReact/> React
+               <SiJquery/> Jquery
+               <SiHeroku/> Heroku
+               <SiGit/> Git
+               <SiNpm/> Npm
+               <SiHandlebarsdotjs/> Handlebars
+                </h1>
+              </div>
     </div>
   );
 }
